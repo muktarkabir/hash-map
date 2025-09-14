4494850062edf9ac3efe1fff5e8e3a8d174303ae
muktarkabir <mukhtarkabir3394@gmail.com>
2025-09-14 02:59:42 +0100

    Create hash map class.

---
+++ hash-map.js
@@ -0,0 +1,25 @@
+class HashMap {
+  constructor() {}
+  _capacity = 16;
+  _loadFactor = 0.75;
+
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+
+    return hashCode;
+  }
+  set(key, value) {}
+  get(key) {}
+  has(key) {}
+  remove(key) {}
+  length() {}
+  clear() {}
+  keys() {}
+  values() {}
+  entries() {}
+}
